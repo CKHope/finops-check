d41e22caf5732e113955bb94bc73b873bf6b6c28
CKHope <chungkien20211011@gmail.com>
2024-07-01 16:21:45 +0700

    ud

--- pages/1__📲deposit.py
+++ pages/1__📲deposit.py
@@ -56,9 +56,9 @@ if deposit_file:
     tolerance_inputs = {
         'RC_CLEO.Lit Sell GDR/USD - Reference': 1e-2,
         'RC_Deposit Amount USD': 1e-10,
-        'RC_GDR Client Receive': 1e-10,
-        'RC_COGs': 1e-10,
-        'RC_Revenue': 1e-10,
+        'RC_GDR Client Receive': 1e-2,
+        'RC_COGs': 1e-2,
+        'RC_Revenue': 1e-2,
         'RC_Mark up rate 5 - Value - Transfer transasaction & gas fee': 1e-2,
         'RC_Mark up rate 4 - Value - Business risk reserve': 1e-2,
         'RC_Mark up rate 3 - Value - Crypto to fiat conversion': 1e-2,
